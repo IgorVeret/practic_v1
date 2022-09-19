9b10311579be88dfc8e6e7c098c96d18b1c384e1
VeretennikovIE <iveretennikov77@gmail.com>
2022-09-19 17:28:31 +0300

    Done

--- lab_03/task_01.py
+++ lab_03/task_01.py
@@ -6,12 +6,19 @@ b = np.array([1, -2, -3, 4, 5, 6])
 
 
 def task_1(arr):
+    """
+
+        Функция проверки массива на положительность
+        :param year: - массив
+        :return: - True or False
+    """
+
     new = arr > 0
     return new.all()  # Обобщаем список
 
 
 if __name__ == "__main__":
-    arr_a=task_1(a)
-    arr_b=task_1(b)
+    arr_a = task_1(a)
+    arr_b = task_1(b)
     print(arr_a)
     print(arr_b)
